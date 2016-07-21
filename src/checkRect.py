2381a17b1c2f3fdfdc1839fa4022fed882744ea2
Caleb Trotz <caleb.trotz@gmail.com>
2016-07-21 16:02:05 -0400

    newest checkrect

--- src/checkRect.py
+++ src/checkRect.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import Float32
+from std_msgs.msg import Float32, Bool, Int32
 from sensor_msgs.msg import Image
 
 class checkRect:
@@ -12,6 +12,11 @@ class checkRect:
                		Image, self.callback)
 		self.error_pub = rospy.Publisher("/error",\
 			Float32, queue_size=3)		
-		
+		self.done_pub = rospy.Publisher("/rect_done",Bool, queue_size=1)
+		self.color_pub = rospy.Publisher("/color", Int32, queue_size=1)
+
 		self.flag = True
+		self.color = None # 0 is red, 1 is green
+
+		
 		
