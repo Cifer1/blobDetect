69609132b18b4e80cb3934adc89f52a079045bbf
cifer1 <caleb.trotz@gmail.com>
2016-07-25 15:16:22 +0000

    final from 45

--- src/checkRect.py
+++ src/checkRect.py
@@ -14,25 +14,27 @@ class checkRect:
 		# init the pubs and subs
 		self.img_sub = rospy.Subscriber("/camera/rgb/image_rect_color",\
                		Image, self.cbImage)
+		self.img_pub = rospy.Publisher("/echo",Image, queue_size = 1)
 		self.angle_pub = rospy.Publisher("/steer_angle",\
 			Float32, queue_size=3)		
 		self.done_pub = rospy.Publisher("/rect_done",Bool, queue_size=1)
 		self.color_pub = rospy.Publisher("/color", Int32, queue_size=1)
-
+		self.bridge = CvBridge()
+		self.delivered = False
 		self.flag = True
 		self.color = None # 0 is red, 1 is green
 
-		self.RED_LOWER = np.array([7,100,180])
-		self.RED_UPPER = np.array([15,255,255])
-		self.GRN_LOWER = np.array([67,100,100])
+		self.RED_LOWER = np.array([0,130,130])
+		self.RED_UPPER = np.array([10,255,255])
+		self.GRN_LOWER = np.array([60,100,100])
 		self.GRN_UPPER = np.array([77,255,255])
 		self.final_lower = None
 		self.final_upper = None
-
-        def cbImage(self,image_msg):
-		if self.flag == False:
-			rospy.signal_shutdown("Done with task (checkRect)")
-			return
+		self.thread_lock = threading.Lock()
+	def cbImage(self,image_msg):
+		if not self.delivered:
+			self.cbImageH(image_msg)
+        def cbImageH(self,image_msg):
 		rospy.loginfo("cbImage")        
 		thread = threading.Thread(target=self.processImage,args=(image_msg,))
 		thread.setDaemon(True)
@@ -41,17 +43,25 @@ class checkRect:
    	def contour(self, image_cv):
 		hsv = cv2.cvtColor(image_cv, cv2.cv.CV_BGR2HSV)	# convert to hsv
 		if self.color == None:
+			rospy.loginfo(str(self.flag))
+			binMask = cv2.inRange(hsv, self.RED_LOWER, self.RED_UPPER) # take red mask
+			binMask2 = cv2.inRange(hsv, self.GRN_LOWER, self.GRN_UPPER) # take green mask 
 
-			binMask = cv2.inRange(hsv, RED_LOWER, RED_UPPER) # take red mask
-			binMask2 = cv2.inRange(hsv, GRN_LOWER, GRN_UPPER) # take green mask 
-
-			contours_red, _  = cv2.findContours(binMask, cv2.cv.CV_RETR_EXTERNAL, cv2.cv.CV_CHAIN_APPROX_SIMPLE) # red contour
-			contours_grn, _ = cv2.findContours(binMask2, cv2.cv.CV_RETR_EXTERNAL, cv2.cv.CV_CHAIN_APPROX_SIMPLE) # green contour
-			
-			for c in contours_red: 
-				if cv2.contourArea(c) > 2000: 
-					self.color == 0 
-				else: self.color == 1 # if big red thing is there, red is color, else green
+			contours_red,_  = cv2.findContours(binMask, cv2.cv.CV_RETR_EXTERNAL, cv2.cv.CV_CHAIN_APPROX_SIMPLE) # red contour
+			contours_grn,_ = cv2.findContours(binMask2, cv2.cv.CV_RETR_EXTERNAL, cv2.cv.CV_CHAIN_APPROX_SIMPLE) # green contour		
+			flag = "Green"
+			print "color contours taken"
+			bigR = self.pickBiggest(contours_red) if len(contours_red) > 0 else None
+			bigG = self.pickBiggest(contours_grn) if len(contours_grn) > 0 else None
+			if bigR != None and bigG != None and cv2.contourArea(bigR) > cv2.contourArea(bigG):
+				self.color = 0
+			elif bigR != None and bigG != None and cv2.contourArea(bigG) > cv2.contourArea(bigR):
+				self.color = 1
+			elif bigR != None:
+				self.color = 0
+			elif bigG != None:
+				self.color = 1
+			self.color = 1
 			if self.color == 0: # sets mask bounds for red
 				self.final_lower = self.RED_LOWER 
 				self.final_upper = self.RED_UPPER
@@ -59,42 +69,62 @@ class checkRect:
 				self.final_lower = self.GRN_LOWER
 				self.final_upper = self.GRN_UPPER
 			self.color_pub.publish(self.color)
-
+			
+		print "color " + str(self.color)
 		binMask = cv2.inRange(hsv, self.final_lower, self.final_upper) # binary mask
 		masked = cv2.bitwise_and(hsv,hsv,mask=binMask) 
 	
 		contours, _ = cv2.findContours(binMask,cv2.cv.CV_RETR_EXTERNAL,cv2.cv.CV_CHAIN_APPROX_SIMPLE) # contouring
-		print len(contours)
-		for c in contours:
-			#print "contour loop"
-			x,y,w,h = cv2.boundingRect(c) # find the rect around the contour
-			if w*h < 2000: # if too small, pay no attention
-				#print "cont'd"
-				continue
-			elif w*h > 1e5: # if too big, end the node
-			
-				self.flag == False
-				continue
-			msg = x + 0.5 * w # finds x val of center of contour 
-			#cv2.rectangle(image_cv, (x,y),(x+w, y+h),(0,255,0),2)
+		#print len(contours)
+		msg = 0
+		c = self.pickBiggest(contours) if len(contours)>0 else None
+		x,y,w,h = cv2.boundingRect(c)
+		if w*h>20000:
+			print "big"
+			self.flag = False
 
-		if self.flag == False: # if we're done
+		#for c in contours:
+			#print "contour loop"
+			#x,y,w,h = cv2.boundingRect(c) # find the rect around the contour
+			#if w*h < 2000: # if too small, pay no attention
+				#print "cont'd"
+				#continue
+			#elif h > 300: # if too big, end the node
+				#print "big"
+				#self.flag = False
+				#continue
+		msg = x + 0.5 * w # finds x val of center of contour 
+			#cv2.rectangle(image_cv, (x,y),(x+w, y+h),(0,255,0),2)
+		print str(msg)
+		if self.flag == False and self.delivered == False: # if we're done
 			self.done_pub.publish(self.flag) # say we're done
-			return
+			rospy.loginfo(str(self.flag))
+			self.delivered = True
+			
 		error = msg - 640 # distance from center
-		self.angle_pub.publish(error * -0.01) # publishes P controlled angle for driving 
-		
+		if abs(error) != 640:
+			self.angle_pub.publish(error * -0.001) # publishes P controlled angle for driving 
+
+		self.img_pub.publish(self.bridge.cv2_to_imgmsg(image_cv))
 		
 
 		#cv2.imshow("masked",image_cv)
 		#cv2.waitKey(0)
 		#cv2.destroyAllWindows()
 	
-	
+	def pickBiggest(self,contours):
+		result = contours[0]
+		for x in contours:
+			if cv2.contourArea(x)>cv2.contourArea(result):
+				result = x
+		return result
         def processImage(self, image_msg):
+		print "processIm"
 		if not self.thread_lock.acquire(False):
+		    #print("failed")
 		    return
 		image_cv = self.bridge.imgmsg_to_cv2(image_msg)
+		#print "about to try"
 		try:
 			print "tried"            
 			self.contour(image_cv)
--- src/mainNode.py
+++ src/mainNode.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from ackermann_msgs.msg import AckermannDriveStamped
+from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Int32, Bool, Float32
 #from sensor_msgs.msg import
 from blobDetectorForDrive import Echo
@@ -15,51 +16,72 @@ class mainNode:
 		self.drive_pub = rospy.Publisher("/vesc/ackermann_cmd_mux/input/navigation", AckermannDriveStamped, queue_size = 1)
 		self.color_sub = rospy.Subscriber("/color", Int32, self.colorCB)
 		self.rectdone_sub = rospy.Subscriber("/rect_done", Bool, self.step3)
-		self.angle_sub = rospy.Subscriber("/steer_angle", Float32, self.driveUpdate)
-		self.start_pub = rospy.Publisher("/follow_start", Bool, queue_size = 1)
+		self.angle1_sub = rospy.Subscriber("/steer_angle", Float32, self.driveUpdate1)
+		self.angle2_sub = rospy.Subscriber("/steer_angle_follow", Float32, self.driveUpdate2)
+		self.scan_sub = rospy.Subscriber("/scan", LaserScan, self.scan)
 		self.side_pub = rospy.Publisher("/side", Int32, queue_size=1)
 		self.drive_msg = AckermannDriveStamped()
-		self.drive_msg.drive.speed = 1.0		
-		
+		self.drive_msg.drive.speed = 1.0
+		self.scanVal = 0	
+		self.back = False	
+		self.angle_flag = 1
+		self.runStep = True
 		self.color = None
 		self.drive()
 
 	def drive(self):
 		while not rospy.is_shutdown():			
 			self.drive_pub.publish(self.drive_msg)
-			rospy.Rate(4).sleep()
-
-	def driveUpdate(self, msg):
-		self.drive_msg.drive.steering_angle = msg
-
+			print(self.drive_msg.drive.steering_angle)
+			print(self.drive_msg.drive.speed)
+			rospy.Rate(8).sleep()
+	def scan(self, msg):
+		self.scanVal = msg.ranges[540]
+		if self.back and self.scanVal < 1.5:					
+			self.drive_msg.drive.steering_angle = 0.0
+			self.drive_msg.drive.speed = -0.5
+		elif self.back:
+			self.back = False
+			self.step3H()
+			
+	def driveUpdate1(self, msg):
+		if self.angle_flag == 1:
+			self.drive_msg.drive.steering_angle = msg.data
+	def driveUpdate2(self, msg):
+		if self.angle_flag == 2:
+			self.drive_msg.drive.steering_angle = msg.data
 	def colorCB(self, msg):
-		self.color = msg
+		self.color = msg.data
 		if self.color == 0: print "red"
 		else: print "green"
 		
-
 	def step3(self,msg):
-		self.drive_msg.drive.speed = 0.0
-		time = rospy.Time.now()
-		while rospy.Time.now() - time < 1:
-			self.drive_msg.drive.speed = -1.0
-		self.drive_msg.drive.speed = 0.0			
-		if self.color == 0:
-			self.drive_msg.drive.steering_angle = 1.0
-		else:
-			self.drive_msg.drive.steering_angle = -1.0
-		time = rospy.Time.now()		
-		while rospy.Time.now() - time < 1:
-			self.drive_msg.drive.speed = 1.0
-		self.drive_msg.drive.speed = 0.0
-		self.step4()
+		if self.runStep == True:
+			#self.step3H()
+			self.back = True
+	def step3H(self):
+			print("in step 3H")
+
+			if self.color == 0:
+				self.drive_msg.drive.steering_angle = -1.0
+			else:
+				self.drive_msg.drive.steering_angle = -1.0
+			time = rospy.Time.now()		
+			while rospy.Time.now() - time < rospy.Duration(1.5):
+				self.drive_msg.drive.speed = 1.0
+			self.drive_msg.drive.speed = 0.0
+			self.drive_msg.drive.steering_angle = 0.0
+			self.runStep = False
+			
+			self.step4()
 	
 	def step4(self):
+		print("in step 4")
 		if self.color == 0:
 			self.side_pub.publish(1)
 		else:
-			self.side_pub.publish(2)		
-		self.start_pub.publish(True)
+			self.side_pub.publish(2)
+		self.angle_flag = 2
 		self.drive_msg.drive.speed = 1.0
 		
 		
